f3895bb14ba032b5e2a162140a011eab5bb03976
Carlos Aguirre <carlosanibal815@gmail.com>
2026-02-04 22:01:17 -0300

    mejora card y calculo total

--- js/carrito.js
+++ js/carrito.js
@@ -5,17 +5,31 @@ let cardCarrito = document.getElementById("card-carrito")
 
 
 function renderizarCarrito (cardItems) {
+    const row = document.createElement("div")
+    row.className = "row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mb-4"
+    cardCarrito.appendChild(row)
     cardItems.forEach(article => {
-        cardCarrito.classList.add("row", "g-3")
         const card = document.createElement("div")
-        card.className = "card border-dark col-12 col-sm-6 col-md-4 mb-3 p-3 mb-2 bg-light text-dark"
-        card.innerHTML = `  
-                        <div class="card-body">
-                            <h3 class="card-header">${article.nombre}</h3>
-                            <h4 class="card-text">Precio: $${article.precio}</h4>
-                            <h4 class="card-text">Categoria: ${article.deporte}</h4>
-                        </div>`
-        cardCarrito.appendChild(card)
+        card.className = "col"
+
+        const totalCarrito = document.getElementById("totalCarrito")
+        const total = cardItems.reduce((sum, item) => sum + item.precio, 0)
+        totalCarrito.textContent = `Total: $${total}`
+
+        card.innerHTML = `
+            <div class="card h-100 border-dark bg-light text-dark">
+                <div class="card-body d-flex flex-column justify-content-between text-center">
+                    <h3 class="card-header">${article.nombre}</h3>
+                    <h4 class="card-text">Precio: $${article.precio}</h4>
+                    <h4 class="card-text">Categoria: ${article.deporte}</h4>
+                    <button class="articleRemove btn btn-secondary" id="${article.id}">
+                        Quitar del Carrito
+                    </button>
+                </div>
+            </div>
+        `
+        row.appendChild(card)
+
     })
 }
 
